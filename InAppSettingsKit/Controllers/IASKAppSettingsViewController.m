6464866176f45fb95ba2b8d1e5354b8657c8cc46
Philippe Peirano <philippe@peirano.se>
2017-09-25 13:14:22 +0200

    Destructive adaptation. Removed uialertview because they are not allowed in app extensions.

--- InAppSettingsKit/Controllers/IASKAppSettingsViewController.m
+++ InAppSettingsKit/Controllers/IASKAppSettingsViewController.m
@@ -888,6 +888,8 @@ CGRect IASKCGRectSwap(CGRect rect);
 			 */
 			
         } else {
+			/* FIXME PHIPER 2017-09-25 removed because not available in app extension
+
 			IASK_IF_PRE_IOS8
 			(
             UIAlertView *alert = [[UIAlertView alloc]
@@ -906,6 +908,7 @@ CGRect IASKCGRectSwap(CGRect rect);
 			 [alert addAction:[UIAlertAction actionWithTitle:NSLocalizedString(@"OK", @"InAppSettingsKit") style:UIAlertActionStyleCancel handler:^(UIAlertAction * action) {}]];
 			 [self presentViewController:alert animated:YES completion:nil];
 			)
+			 */
         }
 		
     } else if ([[specifier type] isEqualToString:kIASKCustomViewSpecifier] && [self.delegate respondsToSelector:@selector(settingsViewController:tableView:didSelectCustomViewSpecifier:)]) {
